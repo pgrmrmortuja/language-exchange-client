fe3c44576850cca3d3e9bdf5a6ec84717172e36c
pgrmrmortuja <moutasimmortuja@gmail.com>
2025-03-15 15:41:14 +0600

    choose tutor in motoin way

--- src/components/LanguageTuo/CatgSection.jsx
+++ src/components/LanguageTuo/CatgSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { motion } from "framer-motion";
 import { Link } from 'react-router-dom';
 import { FaChevronRight } from "react-icons/fa6";
-
 import { FaChalkboardTeacher } from "react-icons/fa";
 import { GiTeacher } from "react-icons/gi";
 import { PiChalkboardTeacher } from "react-icons/pi";
@@ -15,13 +15,24 @@ import { LiaChalkboardTeacherSolid } from "react-icons/lia";
 const CatgSection = () => {
     return (
         <div className='mb-20'>
-            <h1 className="text-4xl font-bold mb-10 text-center">Choose Your Tutor</h1>
-            
+            <motion.h1
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+                viewport={{ once: false, amount: 0.2 }}
+                className="text-4xl font-bold mb-10 text-center"
+            >Choose Your Tutor</motion.h1>
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mb-5'>
-                
+
                 {/* Card 1 */}
                 <Link to={"/find-tutors/English"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <FaChalkboardTeacher className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">English Tutor</span>
@@ -29,11 +40,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 2 */}
                 <Link to={"/find-tutors/Spanish"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <GiTeacher className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Spanish Tutor</span>
@@ -41,11 +57,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 3 */}
                 <Link to={"/find-tutors/French"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacher className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">French Tutor</span>
@@ -53,11 +74,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 4 */}
                 <Link to={"/find-tutors/German"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacherBold className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">German Tutor</span>
@@ -65,11 +91,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 5 */}
                 <Link to={"/find-tutors/Italian"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacherDuotone className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Italian Tutor</span>
@@ -77,11 +108,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 6 */}
                 <Link to={"/find-tutors/Portuguese"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacherFill className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Portuguese Tutor</span>
@@ -89,11 +125,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 7 */}
                 <Link to={"/find-tutors/Russian"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacherLight className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Russian Tutor</span>
@@ -101,11 +142,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 8 */}
                 <Link to={"/find-tutors/Mandarin"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <PiChalkboardTeacherThin className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Mandarin Tutor</span>
@@ -113,11 +159,16 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
                 {/* Card 9 */}
                 <Link to={"/find-tutors/Japanese"}>
-                    <div className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
+                    <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        className="border-2 border-green-500 p-5 flex justify-between items-center hover:shadow-lg transition-shadow duration-300">
                         <div className="flex items-center space-x-3">
                             <LiaChalkboardTeacherSolid className="text-3xl text-blue-500" />
                             <span className="font-semibold text-lg">Japanese Tutor</span>
@@ -125,7 +176,7 @@ const CatgSection = () => {
 
                         <FaChevronRight className="text-xl text-gray-500" />
 
-                    </div>
+                    </motion.div>
                 </Link>
 
 
